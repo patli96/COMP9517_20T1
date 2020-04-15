74dd08f7a340aa2cd5ffc08b213072d538918072
alexander <alexander@deepyoda.com>
2020-04-15 21:48:55 +1000

    added background subtraction notebook and preprocessor

---
+++ pedestrian_monitor/clusterers/distance_clusterer.py
@@ -0,0 +1,31 @@
+from typing import Dict, Tuple, List
+
+import numpy as np
+
+
+def compute(  # This function will be called with named parameters, so please do not change the parameter name
+        pedestrians: Dict[int, Tuple[int, int, int, int]],  # Dict{ pedestrian_id: (y1, x1, y2, x2), ... }
+        pedestrian_records: List[Dict[int, Tuple[int, int, int, int]]],  # List[ pedestrians ], the current is [0]
+        pedestrian_frame_deltas: List[int],  # List[ pedestrian_frame_delta ], the current is [0]
+        tracks: Dict[int, List[Tuple[int, int]]],  # Dict{ pedestrian_id: [(y, x), ...], ... }
+        # There is no track_records as each (y, x) is a record, and tracks have all pedestrians' tracks
+        # Tracks of those who disappeared or moved out of the image will be removed, and they're useless
+        # Both tracks and pedestrians shared the same frame_delta_records as they both came from trackers
+        image: np.ndarray,  # The image, it is 3-channel BGR uint8 numpy array
+        image_records: List[np.ndarray],  # List[ images ], previously displayed images
+        frame_delta: int,  # current_frame_index - last_computed_frame_index, will be >= 1
+        previous_group_records: List[Dict[int, List[int]]],  # List[ groups ], previously computed groups
+        previous_group_frame_deltas: List[int],  # List[ frame_delta ], for previously computed groups
+) -> Dict[int, List[int]]:
+    # groups is a dict
+    # its indexes are group ids, which should be stable between frames
+    # the group id does not need to start at 0, but it needs to be unique and stable
+    # Example: 0: [0, 1] -> 0: [0, 1, 8] -> 0: [1, 8]
+    # its values are lists that contain >=2 unique pedestrian id
+    # Example: [1] is not a group and should not be returned
+    groups = {
+        0: [0, 1],
+        1: [2, 6, 4],
+        2: [5, 3],
+    }
+    return groups
---
+++ pedestrian_monitor/preprocessors/background_subtraction.py
@@ -0,0 +1,31 @@
+from typing import Tuple, List, Dict, Any
+
+import numpy as np
+
+from ..console_arguments import get_args
+from ..file_handlers import ImageFileIterator
+
+def compute(  # This function will be called with named parameters, so please do not change the parameter name
+        image: np.ndarray,  # The image, it is 3-channel BGR uint8 numpy array
+        image_index: int,  # The current index of frame, started at 0
+        frame_delta: int,  # current_frame_index - last_computed_frame_index, will be >= 1
+        image_records: List[np.ndarray],  # List[ images ], previously displayed images
+        previous_feature_records: List[np.ndarray],  # List[ images ], previously displayed images
+        previous_feature_frame_deltas: List[int],  # List[ frame_delta ], for previously computed detections
+        storage: Dict[str, Any],  # It will be handed over to the next preprocessor, please mutate this object directly
+) -> np.ndarray:
+    # features is a numpy array
+    # It will be used in detectors for pedestrian detection.
+    # To skip the whole preprocess, you may simply return a features
+
+    if 'imgs_modes' not in storage:
+        args = get_args()
+        images = ImageFileIterator(args.path, False)
+        imgs_array = np.array([img[0] for img in images])
+        imgs_modes = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=imgs_array)
+        storage['imgs_modes'] = imgs_modes
+
+
+    sub = np.subtract(image, storage['imgs_modes'])
+    sub = (sub - sub.min()) / (sub.max() - sub.min())
+    return sub.reshape(image.shape)
